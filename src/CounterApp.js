45edbd32dccc96ef409f1902cb35b384cc5274ed
pedro <pedro.donoso.r@usach.cl>
2022-01-20 14:26:08 -0300

    agrego counter

---
+++ src/CounterApp.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+const CounterApp = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+    <h1>CounterApp</h1>  
+    </div>
+  )
+}
+
+export default CounterApp
