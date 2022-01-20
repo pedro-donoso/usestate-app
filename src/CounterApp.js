73c1fdacfd6e11b6aed43eb03b2725965c2cd4ad
pedro <pedro.donoso.r@usach.cl>
2022-01-20 14:54:03 -0300

    agrego boton

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -4,7 +4,10 @@ const CounterApp = () => {
   const [counter, setCounter] = useState(0);
   return (
     <div>
-    <h1>CounterApp</h1>  
+    <button onClick={()=>setCounter(counter+1)}>
+      Incrementar
+    </button>  
+    <h1>Clicks:{counter}</h1>  
     </div>
   )
 }
