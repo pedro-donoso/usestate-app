36c3fae0ce18a529a8b204494c66b73c6254462f
pedro <pedro.donoso.r@usach.cl>
2022-01-20 15:40:40 -0300

    clicks

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,10 +2,15 @@ import React, {useState} from 'react';
 
 const CounterApp = () => {
   const [counter, setCounter] = useState(0);
+
+  const incrementCounter=()=>{
+    setCounter(counter+1)
+  }
+
   return (
     <div>
-    <button onClick={()=>setCounter(counter+1)}>
-      Incrementar
+    <button onClick={incrementCounter}>
+      Increment
     </button>  
     <h1>Clicks:{counter}</h1>  
     </div>
