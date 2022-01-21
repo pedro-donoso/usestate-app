01bfe88bae9d2701db724abe70894b3f05488b39
pedro <pedro.donoso.r@usach.cl>
2022-01-21 11:41:18 -0300

    agrego JSON

--- src/ProductApp.js
+++ src/ProductApp.js
@@ -8,19 +8,21 @@ const initialProduct = {
 const ProductApp=()=>{
   const [product, setProduct]=useState(initialProduct);
 
-  const updateProduct=()=>{
+  const updateProduct=(property, value)=>{
     setProduct({
-      ...product
+      ...product,
+      [property]: value
     })
   }
 
   return (
     <div>
-    <button onClick={updateProduct}>
+    <button onClick={()=>updateProduct("description", "Nuevo desc")}>
       Update
     </button>
       <h1>{product.title}</h1>
       <h1>{product.description}</h1>
+      <div>{JSON.stringify(product)}</div>
     </div>
   )
 }
