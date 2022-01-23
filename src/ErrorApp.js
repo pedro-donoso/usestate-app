96fb64d7001e728b48bce3b077e57b5381857b11
pedro <pedro.donoso.r@usach.cl>
2022-01-23 20:53:20 -0300

    funciona

--- src/ErrorApp.js
+++ src/ErrorApp.js
@@ -6,13 +6,13 @@ const ErrorApp = () => {
 
   return (
     <div className='mx-3'>
-    <button onClick={()=>setError("Error desde la API")} className='btn btn-sm btn-danger'>
+    <button onClick={()=>setError("Error desde la API")} className='btn btn-sm btn-danger mb-3'>
     Mostrar Error
     </button>
-    <button onClick={()=>setError("Credenciales incorrectas")} className='btn btn-sm btn-dark mx-4'>
+    <button onClick={()=>setError("Credenciales incorrectas")} className='btn btn-sm btn-dark mx-4 mb-3'>
     Otro Error
     </button>
-    <button onClick={()=>setError("")} className='btn btn-sm btn-success'>
+    <button onClick={()=>setError("")} className='btn btn-sm btn-success mb-3'>
     Proceso exitoso
     </button>
       {error && <h1>{error}</h1>}
